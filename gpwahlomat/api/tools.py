3ef35f0e85d25fcd4eb8c91bb22978eecd160cb8
adam <adam.p.bannister@gmail.com>
2016-08-18 23:31:18 +0200

    switch format of tools somewhat

--- gpwahlomat/api/tools.py
+++ gpwahlomat/api/tools.py
@@ -11,14 +11,21 @@ db = database_connection()
 cursor = db.cursor()
 
 
+def flatten(_list):
+    return [item for sublist in _list for item in sublist]
+
+
 def get_everything():
     alles = {}
     cursor.execute('''SELECT * FROM statisch ''')
     alles['tenor'] = cursor.fetchall()
-    cursor.execute('''SELECT * FROM kategorie ''')
-    alles['categories'] = cursor.fetchall()
-    cursor.execute('''SELECT * FROM partei ''')
-    alles['parties'] = cursor.fetchall()
-    cursor.execute('''SELECT * FROM frage ''')
-    alles['questions'] = cursor.fetchall()
+    cursor.execute('''SELECT data FROM kategorie ''')
+    alles['categories'] = flatten(cursor.fetchall())
+    cursor.execute('''SELECT data FROM partei ''')
+    alles['parties'] = flatten(cursor.fetchall())
+    cursor.execute(
+        '''SELECT frage.id, frage.data, frage.kategorie_id,
+            antwort.data, antwort.wahl FROM frage, antwort''')
+    alles['questions'] = flatten(cursor.fetchall())
+    print(alles)
     return alles

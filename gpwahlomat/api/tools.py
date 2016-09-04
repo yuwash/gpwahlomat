57614addd2a782f4c9cdda151aab0a5e497a4f53
Julius Schröder <julius-schroeder@posteo.de>
2016-09-04 14:32:47 +0200

    fix look of response data in json for ext ui

--- gpwahlomat/api/tools.py
+++ gpwahlomat/api/tools.py
@@ -6,7 +6,6 @@ A collection of tools used in other modules
 '''
 from db.db_client import database_connection
 
-
 db = database_connection()
 cursor = db.cursor()
 
@@ -17,36 +16,74 @@ def flatten(_list):
 
 def get_everything():
     alles = {}
+
     cursor.execute('''SELECT * FROM statisch ''')
     alles['tenor'] = cursor.fetchall()
-    cursor.execute('''SELECT data FROM kategorie ''')
-    alles['categories'] = flatten(cursor.fetchall())
-    cursor.execute('''SELECT data FROM partei ''')
-    alles['parties'] = flatten(cursor.fetchall())
+
+    cursor.execute('''SELECT id, data FROM kategorie ''')
+    kategories = cursor.fetchall()
+    k_list = []
+
+    for kategorie in kategories:
+        k_dict = {}
+        k_dict[kategorie[0]] = {
+            'id': kategorie[0],
+            'name': kategorie[1]
+        }
+
+        print(k_dict[kategorie[0]])
+        k_list.append(k_dict[kategorie[0]])
+
+    alles['categories'] = k_list
+
+    cursor.execute('''SELECT id, data FROM partei ''')
+    parties = cursor.fetchall()
+    p_list = []
+
+    for partie in parties:
+        p_dict = {}
+        p_dict[partie[0]] = {
+            'id': partie[0],
+            'name': partie[1]
+        }
+
+        print(p_dict[partie[0]])
+        p_list.append(p_dict[partie[0]])
+
+    alles['parties'] = p_list
+
     alles['questions'] = get_questions()
     return alles
 
 
 def get_questions():
-    cursor.execute(
-        '''SELECT frage.id, frage.data, frage.kategorie_id,
-            antwort.data, antwort.wahl FROM frage, antwort''')
-    questions = cursor.fetchall()
-    q_dict = {}
     q_list = []
+
+    cursor.execute(
+        "SELECT frage.id, frage.kategorie_id, frage.data FROM frage")
+    questions = cursor.fetchall()
+
+
     for question in questions:
-        if question[1] in q_dict:
-            q_dict[question[1]]['positions'].append({
-                'orientation':question[4], 'argumentation': question[3]})
-        else:
-            q_dict[question[1]] = {
-                'id': question[0],
-                'cat_id': question[2],
-                'positions': [
-                    {'orientation': question[4], 'argumentation': question[3]}]
-            }
-    for key in q_dict:
-        new_dict = q_dict[key]
-        new_dict['text'] = key
-        q_list.append(new_dict)
+        q_dict = {}
+
+        # save question in q_dict
+        q_dict[question[0]] = {
+            'cat_id': question[1],
+            'text' : question[2],
+            'positions': []
+        }
+
+        # add positions to question
+        cursor.execute(
+                "SELECT antwort.data, antwort.partei_id, antwort.wahl FROM antwort WHERE antwort.frage_id = %s",[question[0]])
+        positions = cursor.fetchall()
+        for position in positions:
+            q_dict[question[0]]['positions'].append({
+                'orientation':position[1],
+                'vote': position[2],
+                'argumentation':position[0]})
+
+        # save question to q_list
+        q_list.append(q_dict)
     return q_list
